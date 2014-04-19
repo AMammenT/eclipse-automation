9ba86ab5d9dc437ccdc3c8eccb45b32099b1d2b8
aruntho <aruntho@cisco.com>
2014-04-19 10:05:59 -0700

    Added setup to hide all folders of child projects.

--- derived-resources.eclipse.js
+++ derived-resources.eclipse.js
@@ -1,7 +1,32 @@
 var allProjects = eclipse.resources.workspace.root.projects;
+
+eclipse.console.println("target Folders will be marked derived.");
+eclipse.console.println("child Projects will be hidden.");
+eclipse.console.println("--------------------------------------");
+
 for (var i=0; i < allProjects.length; i++) {
-    var toMarkFolder = allProjects[i].findMember("target");
-    if (toMarkFolder != null) {
-        toMarkFolder.derived=true;
+
+    // Search for all folders within this project that are named target.  The presence of such a folder indicates
+    // that this project has direct artifacts from a build - only the target folder is marked derived.
+    var targetFolder = allProjects[i].findMember("target");
+    if (targetFolder != null) {
+        targetFolder.derived=true;
+        eclipse.console.println(allProjects[i].name + " - " + targetFolder.name + ": Derived");
+    } else {
+        // Otherwise, look for all children of this project that are folders but not dot files.  Mark them derived
+        // and hidden so they don't even appear in the view.
+        if (allProjects[i].name.search("-parent") != -1) {
+            var childrenOfParents = allProjects[i].members(org.eclipse.core.resources.IContainer.INCLUDE_HIDDEN);
+            for (var j=0; j < childrenOfParents.length; j++) {
+                var candidate = childrenOfParents[j];
+                if ((candidate.type == org.eclipse.core.resources.IResource.FOLDER) &&
+                    (candidate.name.lastIndexOf(".", 0) != 0)) {
+                    candidate.derived = true;
+                    candidate.hidden = true;
+                    eclipse.console.println(allProjects[i].name + " - " + candidate.name + ": Hidden");
+                }
+            }
+        }
     }
 }
+
